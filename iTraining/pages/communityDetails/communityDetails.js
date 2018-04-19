8d6f50a09029755b147e5d4e20d53fb3be60ff4b
fullfamily <2802155264@qq.com>
2018-04-19 16:24:28 +0800

    添加UI界面并code原型代码中训练项目专栏的内容

--- iTraining/pages/communityDetails/communityDetails.js
+++ iTraining/pages/communityDetails/communityDetails.js
@@ -67,6 +67,9 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+    return {
+      title: 'iTraing~',
+      path: '/page/user?id=123'
+    }
   }
 })
--- iTraining/pages/trainingItemList/trainingItemList.js
+++ iTraining/pages/trainingItemList/trainingItemList.js
@@ -1,11 +1,38 @@
 // pages/trainingItemList/trainingItemList.js
+//获取应用实例
+const app = getApp()
+const util = require('../../utils/util.js')
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    scrollleft: 0,  // tab标题的滚动条位置
+    currentTab: 0,  // 预设当前项的值
+    tags: ['力量', '耐力','协调性','水上'],  // 项目分类
+    showItems: util.trainingitem(),
+    currentPages: [],  // 记录当前tab分页查询当前page
+  },
+  // 滑动切换标签样式
+  switchTab: function (e) {
+    console.log('switchTab');
+    let index = e.detail.current  // 分类
+    this.setData({
+      currentTab: index
+    });
+  },
+  // 点击标题切换当前页时改变样式
+  switchNav: function (e) {
+    var cur = e.target.dataset.current;
+    console.log(cur)
+    if (this.data.currentTab == cur) { return false; }
+    else {
+      this.setData({
+        currentTab: cur
+      })
+    };
   },
 
   /**
