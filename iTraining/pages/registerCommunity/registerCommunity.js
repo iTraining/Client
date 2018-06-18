624493a6578e228749c86253c51d6dd77222771f
zwk1091 <zhuwk3@mail2.sysu.edu.cn>
2018-06-18 11:01:08 +0800

    add register community page from yh

--- iTraining/pages/registerCommunity/registerCommunity.js
+++ iTraining/pages/registerCommunity/registerCommunity.js
@@ -7,76 +7,144 @@ Page({
   /**
      * 确认提交
      */
-    
-  submit: function (e) {
-    var that = this;
-    console.log(wx.getStorageSync("set-cookie"))
-
-    
-    if (that.data.CommunityName == undefined || that.data.CommunityDescription == undefined || that.data.CommunityName == "" || that.data.CommunityDescription=="") {
-      console.log("队伍名称或队伍描述不能为空")
-      this.setData({
-        hint:"队伍名称或队伍描述不能为空"
-      })
-      return ;
-    }
-    wx.request({
-      url: 'https://itraining.zhanzy.xyz/api/v1/team',
-      data:Util.json2Form({
-        name: that.data.CommunityName,
-        bio: that.data.CommunityDescription,
-      }),
-      header:{
-        'content-type': 'application/x-www-form-urlencoded',
-        // 'content-type': 'application/json',
-        'Cookie':  wx.getStorageSync("set-cookie")
-      },
-      method: "POST",      
-      success:function(res){
-          console.log("确认成功" + that.data.CommunityName + "---------" + that.data.CommunityDescription);
-          console.log(res.data) 
-          that.setData({
-            hint: "创建成功"
-          })
-      },
-    })
-  },
-
   data: {
-    "CommunityName":'',
-    "CommunityDescription":'',
-    "hint":'',
-    imgUrl:''
+    imagePath: "/image/icon_tab/add.png",
+    reset: false    // 再次添加队伍，后续精进
   },
-  communityNameInput:function(e){
-    this.setData({
-      CommunityName: e.detail.value
-    })
-  },
-  communityDescriptionInput: function (e) {
-    this.setData({
-      CommunityDescription: e.detail.value
-    })
-  },
-  /**
-   * 选择队伍图标
-   */
-  choseTeamIcon:function() {
-    var that=this;
+
+  selectImage: function (e) {
+    let that = this;
     wx.chooseImage({
-      count:1,
-      sizeType:['original','compressed'],
-      sourceType:['album','camera'],
-      success: function(res) {
-        var tempFilePath=res.tempFilePaths;
-        console.log("-----"+res.tempFilePaths);
+      count: 1,
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        var tempFilePaths = res.tempFilePaths
         that.setData({
-          imgUrl:tempFilePath
-          
+          imagePath: res.tempFilePaths[0],
         })
+        console.log(res.tempFilePaths[0])
       },
+
     })
   },
+  formValidate: function (e) {
+    console.log(e);
+    let that = this;
+    if (e.name == "") {
+      wx.showModal({
+        title: '提示',
+        content: '队伍名称不能为空',
+        success: function (res) {
+          if (res.confirm) {
+            console.log('用户点击确定')
+          } else {
+            console.log('用户点击取消')
+          }
+
+        }
+      })
+      return false;
+    }
+    return true;
+  },
+  formSubmit: function (e) {
+    let that = this;
+    console.log('form发生了submit事件，携带数据为:', e.detail.value);
+    if (!that.formValidate(e.detail.value)) {
+      return;
+    } else {
+      console.log(that.data.imagePath),
+        wx.request({
+          url: 'https://itraining.zhanzy.xyz/api/v1/team',
+          filePath: that.data.imagePath,
+          name: "avatar",
+          data: e.detail.value,
+          method: "POST",
+          header: {
+            //'content-type': 'multipart/form-data',
+            // 'content-type': 'application/json',
+            'Cookie': wx.getStorageSync("set-cookie")
+          },
+          success: function (res) {
+            console.log(res.data);
+            that.setData({
+              reset: true
+            })
+          }
+        })
+    }
+  },
+
+  // submit: function (e) {
+  //   var that = this;
+  //   console.log(wx.getStorageSync("set-cookie"))
+
+    
+  //   if (that.data.CommunityName == undefined || that.data.CommunityDescription == undefined || that.data.CommunityName == "" || that.data.CommunityDescription=="") {
+  //     console.log("队伍名称或队伍描述不能为空")
+  //     this.setData({
+  //       hint:"队伍名称或队伍描述不能为空"
+  //     })
+  //     return ;
+  //   }
+  //   wx.request({
+  //     url: 'https://itraining.zhanzy.xyz/api/v1/team',
+  //     data:Util.json2Form({
+  //       name: that.data.CommunityName,
+  //       bio: that.data.CommunityDescription,
+  //     }),
+  //     header:{
+  //       'content-type': 'application/x-www-form-urlencoded',
+  //       // 'content-type': 'application/json',
+  //       'Cookie':  wx.getStorageSync("set-cookie")
+  //     },
+  //     method: "POST",      
+  //     success:function(res){
+  //         console.log("确认成功" + that.data.CommunityName + "---------" + that.data.CommunityDescription);
+  //         console.log(res.data) 
+  //         that.setData({
+  //           hint: "创建成功"
+  //         })
+  //     },
+  //   })
+  // },
+
+  // data: {
+  //   "CommunityName":'',
+  //   "CommunityDescription":'',
+  //   "hint":'',
+  //   imgUrl:''
+  // },
+  // communityNameInput:function(e){
+  //   this.setData({
+  //     CommunityName: e.detail.value
+  //   })
+  // },
+  // communityDescriptionInput: function (e) {
+  //   this.setData({
+  //     CommunityDescription: e.detail.value
+  //   })
+  // },
+  // /**
+  //  * 选择队伍图标
+  //  */
+  // choseTeamIcon:function() {
+  //   var that=this;
+  //   wx.chooseImage({
+  //     count:1,
+  //     sizeType:['original','compressed'],
+  //     sourceType:['album','camera'],
+  //     success: function(res) {
+  //       var tempFilePath=res.tempFilePaths;
+  //       console.log("-----"+res.tempFilePaths);
+  //       that.setData({
+  //         imgUrl:tempFilePath
+          
+  //       })
+  //     },
+  //   })
+  // },
 
   /**
    * 生命周期函数--监听页面加载
