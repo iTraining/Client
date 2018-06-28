a5b08c0f10e2b52be0d8a1f93507d9d7a481a0aa
coder-zyq <15521186330@163.com>
2018-06-28 15:11:31 +0800

    usePage update

--- iTraining/pages/PersonalCenter/PersonalCenter.js
+++ iTraining/pages/PersonalCenter/PersonalCenter.js
@@ -1,5 +1,5 @@
 // pages/PersonalCenter/PersonalCenter.js
-var app=getApp()
+const app=getApp()
 Page({
 
   /**
@@ -43,7 +43,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    userInfo:{}
+    userInfo:{},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
   /**
@@ -61,27 +63,53 @@ Page({
       // Do something when catch error
     }
     
-    var that=this
-    //console.log("on load ")
-    app.getUserInfo(function(userInfo) {
-      // console.log(userInfo)
-      that.setData({
-        userInfo: JSON.parse(userInfo.rawData)
+
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: e.detail.userInfo,
+        hasUserInfo: true
       })
-    })
-    console.log(this.data.userInfo)
+    }
+    else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    }
+    else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+
+
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
+  getUserInfo: function (e) {
+    console.log(e);
+    app.globalData.userInfo = e.detail.userInfo;
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  
   onReady: function () {
   
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
+  
   onShow: function () {
     try {
       var single_trainPlanData = wx.getStorageSync('single_trainPlanData')
@@ -94,23 +122,16 @@ Page({
     }
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
   onHide: function () {
   
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
+  
   onUnload: function () {
   
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
+  
   onPullDownRefresh: function () {
   
   },
