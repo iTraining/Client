9f3717e824e2e0abf1f0d116abd04fecae034256
coder-zyq <15521186330@163.com>
2018-07-05 21:39:43 +0800

    finish and release page get

--- iTraining/pages/SquarePage/square.js
+++ iTraining/pages/SquarePage/square.js
@@ -116,7 +116,7 @@ Page({
 
   TurnToTrainingItemList:function() {
     wx.navigateTo({
-      url: '/pages/complishTrainingPage/complishTrainingPage',
+      url: '/pages/finishItemAndRelease/finishAndRelease',
       success: function(res) {},
       fail: function(res) {},
       complete: function(res) {},
