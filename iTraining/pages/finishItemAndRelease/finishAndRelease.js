943f7451b8e42ed96f1224a30c9e693c64bad24d
fullfamily <2802155264@qq.com>
2018-07-07 15:45:38 +0800

    用户打卡后发布动态页面的格式已完优化

--- iTraining/pages/finishItemAndRelease/finishAndRelease.js
+++ iTraining/pages/finishItemAndRelease/finishAndRelease.js
@@ -8,7 +8,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    imagePath:'../../image/icon_tab/add2.png',
+    imagePath:'../../image/icon_tab/punch_add_image.png',
     completion:'',
     description:'',
     schedule_id:'',
@@ -122,9 +122,6 @@ Page({
           url: '../SquarePage/square',
         })
       },
-      fail:function(res) {
-
-      }
     })
   },
   selectImage: function (e) {
